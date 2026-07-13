9213643de05f9711fd5cc94ffc97435c7312f213
JD-2601 <jdma0308@gmail.com>
2026-07-13 19:59:22 +0530

    Time: 35 ms (100%), Space: 28 MB (54.69%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -1,17 +1,6 @@
 class Solution(object):
     def arrayRankTransform(self, arr):
-        arr1 = sorted(arr)
-        result = []
-        rank = {}
-        n=1
-        for i in range(len(arr1)):
-            if i>0 and arr1[i] == arr1[i-1]:
-                rank[arr1[i]] = n-1
-            else:
-                rank[arr1[i]] = n
-                n=n+1
-        for x in arr:
-            result.append(rank[x])
-        return result
-
+       sorted_unique = sorted(set(arr))
+       rank = {v: i+1 for i,v in enumerate(sorted_unique)}
+       return [rank[x] for x in arr]
             
